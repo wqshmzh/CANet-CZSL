26c9b8f833a75b02b324c88944615ea56cdcccbb
wqshmzh <62829611+wqshmzh@users.noreply.github.com>
2023-03-07 17:08:33 +0800

    Update train.py

--- train.py
+++ train.py
@@ -28,17 +28,6 @@ best_epoch = 0.0
 os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1'
 
 def main():
-    seed = 0
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.enabled = False
-
     # Get arguments and start logging
     print('> Initialize parameters')
     args = parser.parse_args()
